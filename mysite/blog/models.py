de0a9ccaea3f8c5a231c74d3480ab0f1c9e87f54
stognienkovladislav <stognienkovv@gmail.com>
2018-02-06 21:14:00 +0200

    Some tasks

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
     title = models.CharField(max_length = 140)
     body = models.TextField()
     date = models.DateTimeField()
+    comment = models.TextField()
+
 
     def __str__(self):
         return self.title
