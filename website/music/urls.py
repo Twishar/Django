f675f25e0ea398f71f7306909af8a659d3c866e3
stognienkovladislav <stognienkovv@gmail.com>
2018-02-07 21:07:08 +0200

    Django Music website

--- website/music/urls.py
+++ website/music/urls.py
@@ -5,9 +5,13 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.IndexView.as_view(), name = 'index'),
     #album_id
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "detail"),
-    #favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name = "favorite")
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
+    #/music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    #/music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    #/music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.ALbumDelete.as_view(), name='album-delete'),
 ]
