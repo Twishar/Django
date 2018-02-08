2bd1af394397c2c73aebe1404bb716dea6a7f800
stognienkovladislav <stognienkovv@gmail.com>
2018-02-08 12:27:38 +0200

    Registration

--- website/music/urls.py
+++ website/music/urls.py
@@ -6,6 +6,8 @@ app_name = 'music'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name = 'index'),
+
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
     #album_id
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
     #/music/album/add/
@@ -14,4 +16,6 @@ urlpatterns = [
     url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
     #/music/album/2/
     url(r'album/(?P<pk>[0-9]+)/delete/$', views.ALbumDelete.as_view(), name='album-delete'),
+
+
 ]
