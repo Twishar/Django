9fa6428ac88dcbcb43199e56605f01f1e749f019
StognienkoVladislav <stognienkovv@gmail.com>
2018-07-29 21:27:06 +0300

    3 files merge

--- xml_shopping/goods/models.py
+++ xml_shopping/goods/models.py
@@ -7,14 +7,14 @@ class Goods(models.Model):
 
     article_number = models.IntegerField(unique=True)
     title = models.TextField()
-    retail_price = models.IntegerField()
-    weight_per_package = models.IntegerField()
+    price = models.IntegerField()
+    weight_per_package = models.IntegerField(default=0)
     date_of_creation = models.DateField()
     update_date = models.DateField()
-    cost_price = models.IntegerField()
+    retail_price = models.IntegerField()
     category = models.TextField()
-    color = models.TextField()
-    stock = models.TextField()
+    color = models.TextField(default='-')
+    stock = models.TextField(default='-')
 
 
 
--- xml_shopping/goods/views.py
+++ xml_shopping/goods/views.py
@@ -1,11 +1,13 @@
 import csv
+import datetime
 import xml.etree.ElementTree as ET
 
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
-from django.core.files.storage import FileSystemStorage
 from io import TextIOWrapper
 from goods.models import Goods
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.storage import FileSystemStorage
 
 
 @csrf_exempt
@@ -36,37 +38,37 @@ def simple_upload(request):
                 gtin = elem.find('{}gtin'.format(g)).text
                 if gtin in [v[0] for v in data]:
                     prod_type = elem.find('{}product_type'.format(g)).text
+                    price = elem.find('{}price'.format(g)).text
+                    retail_price = elem.find('{}retail-price'.format(g)).text
                     title = elem.find('title').text
-                    color = ''
+
+                    try:
+                        xml_goods = Goods.objects.get(article_number=gtin)
+                        xml_goods.update_date = datetime.datetime.today()
+                    except ObjectDoesNotExist:
+                        xml_goods = Goods()
+                        xml_goods.article_number = gtin
+                        xml_goods.date_of_creation = datetime.datetime.today()
+                        xml_goods.update_date = datetime.datetime.today()
+
+                    xml_goods.title = title
+                    xml_goods.price = price
+                    xml_goods.retail_price = retail_price
+                    xml_goods.category = prod_type
                     for val in data2:
                         if gtin == val[0]:
-                            color = val[2]
-
-                    print(gtin, title, prod_type, color)
-        print(data)
-        print('///////////////')
-        print(data2)
+                            weight_per_package, color, stock = val[1], val[2], val[3]
+                            xml_goods.weight_per_package = weight_per_package
+                            xml_goods.stock = stock
+                            xml_goods.color = color
+                    xml_goods.save()
 
         # Save docs
         # fs = FileSystemStorage()
         # fs.save(myfile1.name, myfile1)
         # fs.save(myfile2.name, myfile2)
         # fs.save(myfile3.name, myfile3)
-        """
-        test_good = Goods()
-        test_good.article_number = 1231
-        test_good.category = 'test'
-        test_good.color = 'red'
-        test_good.cost_price = 12
-        test_good.date_of_creation = '2011-3-12'
-        test_good.retail_price = 11
-        test_good.stock = 'dsfsd sdfds'
-        test_good.title = 'f dsf s '
-        test_good.update_date = '2013-4-23'
-        test_good.weight_per_package = 13253
-        print('Test')
-        test_good.save()
-        """
+
         return render(request, 'goods/simple_upload.html')
     return render(request, 'goods/simple_upload.html')
 
@@ -92,7 +94,7 @@ def test_data(request):
 
 def check_data(request):
     if request.method == 'GET':
-        test_good = Goods.objects.get(article_number=1231)
-        print(test_good.color)
+        test_good = Goods.objects.get(article_number=123456)
+        print(test_good.weight_per_package)
 
     return render(request, 'goods/simple_upload.html')
